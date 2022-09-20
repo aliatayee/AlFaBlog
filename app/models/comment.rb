5121fe3da98f732555c0b15af62937f4a978bacf
aliatayee <aliaqaatayee@gmail.com>
2022-09-20 14:07:29 +0430

    Update specs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post'
   belongs_to :author, class_name: 'User'
 
-  def self.update_comments_counter(post_id)
+  after_save :update_comments_counter
+  def update_comments_counter(post_id)
     post = Post.find(post_id)
-    post.comments_counter += 1
-    post.save
+    post.update(comments_counter: post.comments.count)
   end
 end

2374db4a9ad776be67f8b5c603c62b7f46578ea3
aliatayee <aliaqaatayee@gmail.com>
2022-09-22 10:20:40 +0430

    Add request specs

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Post, type: :model do
     test_user = User.create(name: 'Ali', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Full-Stack Developer')
     before { 10.times { Comment.create(post_id: subject, author: test_user, text: 'Hola Tom!') } }
 
-    it '#five_recent_comments should return 5 comments' do
+    it 'five_recent_comments should return 5 comments' do
       expect(subject.five_recent_comments.size).to eql(subject.comments.last(5).size)
     end
   end

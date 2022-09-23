cf28aeacb1458d7d68a619b093a761efe647c8a2
aliatayee <aliaqaatayee@gmail.com>
2022-09-23 20:13:17 +0430

    Add views and styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'John Doe', photo: 'https://gravatar.com/avatar/b960dddd75d9d5477ef84da4d8c8a6e8?s=400&d=robohash&r=x', bio: '
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer
+         took a galley of type and scrambled it to make a type specimen book.
+         It has survived not only five centuries')
+second_user = User.create(name: 'Maryam Safai', photo: 'https://gravatar.com/avatar/fee815fec3c300bd5874b026ef8d558e?s=400&d=robohash&r=x', bio: '
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer
+         took a galley of type and scrambled it to make a type specimen book.
+         It has survived not only five centuries')

6e1e667ac23978165f0a31d6d76b6ce9186678e8
BHDHakim <hakim.bhd@gmail.com>
2022-09-29 14:07:47 +0100

    Update main controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.find(params[:user_id])
+  skip_before_action :verify_authenticity_token
+  # prevent users to perfrom action without authentication
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation, :posts_counter)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
+  end
+
+  def after_sign_out_path_for(_scope)
+    root_path
   end
 end
